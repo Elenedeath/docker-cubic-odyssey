b7c9db8083001a5ce6ab93d0a3f69b1362346f58
Elenedeath Olawir <502132+Elenedeath@users.noreply.github.com>
2026-05-31 17:27:09 +0200

    Update wrapper.sh

--- files/wrapper.sh
+++ files/wrapper.sh
@@ -4,5 +4,7 @@ set -euo pipefail
 
 echo "Starting Cubic Odyssey Dedicated Server via Xvfb and Wine"
 cd /home/cubic/server_files/server
-exec xvfb-run -a --server-args="-screen 0 1280x1024x24 -nolisten tcp" \
-  wine CubicOdysseyServer.exe -log
+
+xvfb-run -a --server-args="-screen 0 1280x1024x24 -nolisten tcp" \
+  wine CubicOdysseyServer.exe -log 2>&1 \
+  | stdbuf -oL sed '/^[[:space:]]*#[[:space:]]*$/d'
